72c2a37f80c909e6ca3781c50adc07391e8d6872
laoderidho <2081007@unai.edu>
2022-12-19 23:05:04 +0700

    update today

--- tesReact/Project1/components/TesButton.js
+++ tesReact/Project1/components/TesButton.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {View, Button, ToastAndroid, Alert} from 'react-native'
+import {View, Text, Button, ToastAndroid, Alert, Linking} from 'react-native'
 
 class TesButton extends Component {
     constructor(props) {
@@ -9,15 +9,17 @@ class TesButton extends Component {
     render() {
         return (
             <View>
+                <Text onPress={()=>Linking.openURL('https://github.com/laoderidho/React_Native_Tutorial')}>Ini adalah Link Github Saya</Text>
                 <Button title='Press me' onPress={()=>ToastAndroid.show(`ini adalah tombol saya`, ToastAndroid.SHORT)}/>
-                <Button title='klik saya' onPress={()=>Alert.alert(`Penting`, `anda mengklik klik saya`, [
+                <Button title='klik saya lagi' onPress={()=>Alert.alert(`Warning`, `apakah anda ingin keluar`, [
                     {
-                        text: 'cancel',
+                        text: `cancel`,
+                        onPress: ``,
                         style: 'cancel'
                     },
                     {
-                        text: 'Ok',
-                        style: 'cancel'
+                        text: `Ok`
+
                     }
                 ])} />
             </View>
