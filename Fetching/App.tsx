fe7452a3c40e5c3f74c7ef855d4cda0ee781d55b
laoderidho <2081007@unai.edu>
2023-03-10 13:40:38 +0700

    update now

---
+++ Fetching/App.tsx
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello Ridho</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
